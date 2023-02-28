15faac3fb3ae023a92624012d768d5ba345541ed
prisc <solanps23@gmail.com>
2023-02-28 13:40:22 +0100

    terminada

--- app/src/main/java/com/example/repasobd/fragments/SettingFragment.java
+++ app/src/main/java/com/example/repasobd/fragments/SettingFragment.java
@@ -6,7 +6,7 @@ import androidx.preference.PreferenceFragmentCompat;
 
 import com.example.repasobd.R;
 //Esta clase sirve para poder añadir las preferencias
-// desde el xml creado anteriormente.
+//desde el xml creado anteriormente.
 public class SettingFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
